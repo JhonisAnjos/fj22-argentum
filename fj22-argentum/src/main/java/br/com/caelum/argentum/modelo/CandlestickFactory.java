40a98e46d5f25fbb94e0c87d94f0563640a9b2a7
jhonis.anjos <jhonis.anjos@NGTIC005441.agerba.intranet>
2018-12-27 16:23:31 -0300

    versão final

--- fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
+++ fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
@@ -1,15 +1,16 @@
 package br.com.caelum.argentum.modelo;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 public class CandlestickFactory {
 	public Candlestick constroiCandleParaData(Calendar data,
 			List<Negociacao> negociacoes) {
-		//double maximo = negociacoes.get(0).getPreco();
-		double maximo=0;
-		//double minimo = negociacoes.get(0).getPreco();
-		double minimo = negociacoes.isEmpty()?0: Double.MAX_VALUE;
+		// double maximo = negociacoes.get(0).getPreco();
+		double maximo = 0;
+		// double minimo = negociacoes.get(0).getPreco();
+		double minimo = negociacoes.isEmpty() ? 0 : Double.MAX_VALUE;
 		double volume = 0;
 
 		for (Negociacao negociacao : negociacoes) {
@@ -17,17 +18,44 @@ public class CandlestickFactory {
 
 			if (negociacao.getPreco() > maximo) {
 				maximo = negociacao.getPreco();
-			} 
+			}
 			if (negociacao.getPreco() < minimo) {
 				minimo = negociacao.getPreco();
 			}
 		}
 
-		double abertura =negociacoes.isEmpty()?0 : negociacoes.get(0).getPreco();
-		double fechamento =negociacoes.isEmpty()?0 : negociacoes.get(negociacoes.size() - 1).getPreco();
+		double abertura = negociacoes.isEmpty() ? 0 : negociacoes.get(0)
+				.getPreco();
+		double fechamento = negociacoes.isEmpty() ? 0 : negociacoes.get(
+				negociacoes.size() - 1).getPreco();
 
 		return new Candlestick(abertura, fechamento, minimo, maximo, volume,
 				data);
 
+	}
+
+	public List<Candlestick> constroiCandles(List<Negociacao> todasNegociacoes) {
+		List<Candlestick> candles = new ArrayList<Candlestick>();
+
+		List<Negociacao> negociacoesDoDia = new ArrayList<Negociacao>();
+		Calendar dataAtual = todasNegociacoes.get(0).getData();
+
+		for (Negociacao negociacao : todasNegociacoes) {
+			// se não for mesmo dia, fecha candle e reinicia variáveis
+			if (!negociacao.isMesmoDia(dataAtual)) {
+				Candlestick candleDoDia = constroiCandleParaData(dataAtual,
+						negociacoesDoDia);
+				candles.add(candleDoDia);
+				negociacoesDoDia = new ArrayList<Negociacao>();
+				dataAtual = negociacao.getData();
+			}
+			negociacoesDoDia.add(negociacao);
+		}
+		// adiciona último candle
+		Candlestick candleDoDia = constroiCandleParaData(dataAtual,
+				negociacoesDoDia);
+		candles.add(candleDoDia);
+
+		return candles;
 	}
 }
